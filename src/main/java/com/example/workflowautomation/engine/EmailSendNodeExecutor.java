82caef77c1ab189ae61c784a5f8f5eb8c961bc71
Jagdish Singh Bisht <jagdishbisht10192@gmail.com>
2026-03-11 11:20:38 +0530

    feat: add JSON-based config parsing for EMAIL_SEND node

--- src/main/java/com/example/workflowautomation/engine/EmailSendNodeExecutor.java
+++ src/main/java/com/example/workflowautomation/engine/EmailSendNodeExecutor.java
@@ -1,10 +1,13 @@
 package com.example.workflowautomation.engine;
 
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.workflowautomation.entity.WorkflowNode;
 import com.example.workflowautomation.service.EmailService;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 
 
 @Component("EMAIL_SEND")
@@ -22,6 +25,26 @@ public class EmailSendNodeExecutor implements NodeExecutor{
 
         String to = "jbisht526@gmail.com";
         String subject = "Automated Notice";
+
+        try{
+
+            if(node.getConfigJson() != null) {
+                ObjectMapper mapper = new ObjectMapper();
+
+                Map<String, String> config =
+                        mapper.readValue(node.getConfigJson(), Map.class);
+
+                if(config.containsKey("to")) {
+                    to = config.get("to");
+                }
+
+                if(config.containsKey("subject")) {
+                    subject = config.get("subject");
+                }
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("Invalid config_json format", e);
+        }
 
         emailService.sendEmail(to, subject, input);
 
