aa6be372a1b71531b0c7ee1e3995594a01f3c651
Jagdish Singh Bisht <jagdishbisht10192@gmail.com>
2026-03-20 11:48:35 +0530

    feat: add Task entity, repository and integrate taskId in workflow request

---
+++ src/main/java/com/example/workflowautomation/dto/WorkflowRunRequest.java
@@ -0,0 +1,22 @@
+package com.example.workflowautomation.dto;
+
+
+import lombok.Data;
+
+
+@Data
+public class WorkflowRunRequest {
+
+    private Long workflowId;
+    private String input;
+
+    // future use
+    private String email;
+    private String taskName;
+    private String description;
+    private String date;
+
+    private Long taskId;
+
+    
+}
---
+++ src/main/java/com/example/workflowautomation/entity/Task.java
@@ -0,0 +1,61 @@
+package com.example.workflowautomation.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDate;
+
+
+
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String taskName;
+
+    private String description;
+
+    private LocalDate taskDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(LocalDate taskDate) {
+        this.taskDate = taskDate;
+    }
+}
