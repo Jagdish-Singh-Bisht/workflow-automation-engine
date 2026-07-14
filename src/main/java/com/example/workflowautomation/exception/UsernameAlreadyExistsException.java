86b89e582e76841723379c2b0b56901af20f7ba6
Jagdish Singh Bisht <jagdishbisht10192@gmail.com>
2026-07-14 16:46:59 +0530

    feat(auth): add UsernameAlreadyExistsException

---
+++ src/main/java/com/example/workflowautomation/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.example.workflowautomation.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
