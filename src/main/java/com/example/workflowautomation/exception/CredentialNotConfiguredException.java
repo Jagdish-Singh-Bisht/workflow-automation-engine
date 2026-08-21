bd7439882dabfad94dcb03f586daea75e3f5aad7
Jagdish Singh Bisht <jagdishbisht10192@gmail.com>
2026-08-21 17:34:21 +0530

    Add workflow exception handling

---
+++ src/main/java/com/example/workflowautomation/exception/CredentialNotConfiguredException.java
@@ -0,0 +1,9 @@
+package com.example.workflowautomation.exception;
+
+public class CredentialNotConfiguredException extends RuntimeException {
+
+    public CredentialNotConfiguredException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/example/workflowautomation/exception/WorkflowConfigurationException.java
@@ -0,0 +1,9 @@
+package com.example.workflowautomation.exception;
+
+public class WorkflowConfigurationException extends RuntimeException {
+
+    public WorkflowConfigurationException(String message) {
+        super(message);
+    }
+
+}
